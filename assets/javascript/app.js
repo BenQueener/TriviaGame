6ea678d448872ed3f15b4cbd1a5d86b5b122514c
BenQueener <47427613+BenQueener@users.noreply.github.com>
2019-03-10 23:36:13 -0500

    Javascript is mostly done.  Need to add countdown timer.  Weird bugs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,98 @@
 
 //global variables
+var questionsIndex = 0;
+var howManyRight = 0;
+var howManyWrong = 0;
+var time = 10;
 
 //data object with all the questions answers and maybe images to display
+var questions = [
+    {   question : "What is an Autobot's favorite type of music?",
+        answers : ["Rock", "Classical", "Country", "Jazz"],
+        correctAnswerIndex : 3
+    },
+    {   question : "What is an Autobot's favorite science TV show",
+        answers : ["Bill Nye the Science Guy", "How It's Made", "Cosmos", "MythBusters"],
+        correctAnswerIndex : 2
+    },
+    {   question : "What is an Autobot's favorite insect?",
+        answers : [ "Bumblebee", "Grasshopper", "Ladybug", "Fly"],
+        correctAnswerIndex : 0
+    }
+];	
 
-	  
+function grabQuestion(){
+    //if we don't have any more questions
+    if (questionsIndex === questions.length){
+        //show the stats
+        $("#gameDisplay").html("<p> You got " + howManyRight + " right and " + howManyWrong + " wrong.");
+        $("#gameDisplay").append("<h1 id='start'>Reset Game?</h1>");
+        //Give them the option to restart the game
+        resetGame();
+        $("#start").click(grabQuestion);
+    }
+    else {
+        //display the question
+        $("#gameDisplay").html("<p> Time Remaining: <span id='timer'>" + time + "</span> seconds </p>");
+        $("#gameDisplay").append("<p>" + questions[questionsIndex].question + "</p>");
+        //now we display the possible answers
+        for (i = 0; i< questions[questionsIndex].answers.length; i++){
+            $("#gameDisplay").append("<p class='options'>" + questions[questionsIndex].answers[i] + "</p>");
+        }
+        //Now the user chooses an answer
+        $("#gameDisplay").on("click", ".options", function() {
+            //we see if the answer chosen matches the number of the right answer.
+            //if the text of the option chosen matches the text of the location of the correct
+            //answer, then....
+            var correctAnswer = questions[questionsIndex].correctAnswerIndex;
+            if ($(this).text() === questions[questionsIndex].answers[correctAnswer]) {
+                // player wins function
+                questionsIndex++;
+                howManyRight++;
+                $("#gameDisplay").html("<h2> Correct! </h2>")
+                setTimeout(grabQuestion, 2000);
+            }
+            //if they choose the wrong answer
+            else {
+                questionsIndex++;
+                howManyWrong++;
+                $("#gameDisplay").html("<h2> Wrong! </h2>")
+                setTimeout(grabQuestion, 2000);
+            }
+        });
+    }
+}
+function resetGame(){
+    time = 10;
+    questionsIndex = 0;
+    howManyRight = 0;
+    howManyWrong = 0;
+}
+
+$(document).ready(function() {
+
+
+    $("#start").click(function(){
+        
+        $("#start").hide();
+        time = 10;
+        //Display the timer at the top
+        grabQuestion();
+
+
+    });
+
+        
+
+       
+});
+
+
+//When we start the game by clicking the start button
+//Then we hide the start button and replace it with the countdown clock
+//then we grab a question from the data object and start the countdown
+
+
 //Dig up a question from our data object and append each of the choices to the game screen 
 // loop through all the possible choices 
 
@@ -29,7 +118,7 @@
 //If the time runs down to zero it clears the timer and does whatever happens 
 //in the game when the time runs out.
 
-//SErve up the next question and start the clocks
+//Serve up the next question and start the clocks
 // moves question counter forward to show next question
 //We are going tocheck and make sure we are not out of questions.
 //We'll do that by checking the questioncounter and making sure that we are not at the last one
@@ -40,9 +129,7 @@
 
 //If we reset the game we restart the glabal variables
 
-//When we start the game by clicking the start button
-//Then we hide the start button and replace it with the countdown clock
-//then we grab a question from the data object and start the countdown
+
 
 
 
